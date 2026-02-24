c67256108a880ab42e6badbf6ad8f1f055bb1c48
KaminK <k.kvvnkm@gmail.com>
2026-02-24 09:49:54 +0800

    Initial Commit - TCP Client Server Prototype

---
+++ TCP_ServerClient_App/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace TCP_ServerClient_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtLogLabel = new System.Windows.Forms.Label();
+            this.txtPortLabel = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.btnStopServer = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.btnStartServer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtLogLabel
+            // 
+            this.txtLogLabel.AutoSize = true;
+            this.txtLogLabel.Location = new System.Drawing.Point(107, 153);
+            this.txtLogLabel.Name = "txtLogLabel";
+            this.txtLogLabel.Size = new System.Drawing.Size(94, 20);
+            this.txtLogLabel.TabIndex = 11;
+            this.txtLogLabel.Text = "Server Log: ";
+            // 
+            // txtPortLabel
+            // 
+            this.txtPortLabel.AutoSize = true;
+            this.txtPortLabel.Location = new System.Drawing.Point(107, 124);
+            this.txtPortLabel.Name = "txtPortLabel";
+            this.txtPortLabel.Size = new System.Drawing.Size(42, 20);
+            this.txtPortLabel.TabIndex = 10;
+            this.txtPortLabel.Text = "Port:";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(207, 121);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(368, 26);
+            this.txtPort.TabIndex = 9;
+            this.txtPort.Text = "3490";
+            // 
+            // btnStopServer
+            // 
+            this.btnStopServer.Enabled = false;
+            this.btnStopServer.Location = new System.Drawing.Point(394, 287);
+            this.btnStopServer.Name = "btnStopServer";
+            this.btnStopServer.Size = new System.Drawing.Size(181, 52);
+            this.btnStopServer.TabIndex = 8;
+            this.btnStopServer.Text = "Stop Server";
+            this.btnStopServer.UseVisualStyleBackColor = true;
+            this.btnStopServer.Click += new System.EventHandler(this.btnStopServer_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(207, 153);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(368, 128);
+            this.txtLog.TabIndex = 7;
+            // 
+            // btnStartServer
+            // 
+            this.btnStartServer.Location = new System.Drawing.Point(207, 287);
+            this.btnStartServer.Name = "btnStartServer";
+            this.btnStartServer.Size = new System.Drawing.Size(181, 52);
+            this.btnStartServer.TabIndex = 6;
+            this.btnStartServer.Text = "Start Server";
+            this.btnStartServer.UseVisualStyleBackColor = true;
+            this.btnStartServer.Click += new System.EventHandler(this.btnStartServer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(960, 569);
+            this.Controls.Add(this.txtLogLabel);
+            this.Controls.Add(this.txtPortLabel);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.btnStopServer);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnStartServer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtLogLabel;
+        private System.Windows.Forms.Label txtPortLabel;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Button btnStopServer;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.Button btnStartServer;
+    }
+}
+
---
+++ TCP_ServerClient_App/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Linq.Expressions;
+
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+//using System.Drawing;
+//using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Remoting.Channels;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TCP_ServerClient_App
+{
+    public partial class Form1 : Form
+    {
+
+        TcpListener server; //represent bind and listen socket in one class, which is more convenient to use but less flexible than using Socket class directly, but for this simple server, it's enough
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStartServer_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //int port = int.Parse(txtPort.Text); // this will fail if port is empty or not a number
+                if (!int.TryParse(txtPort.Text, out int port)) 
+                { 
+                    MessageBox.Show("Please enter a valid port number.");
+                    return;
+                }
+                if (port < 1024) 
+                { 
+                    MessageBox.Show("Please enter a port number greater than 1024 to avoid conflict with Privileged Ports!");  //reference from Beej's Network Programming
+                    return;
+                }
+                server = new TcpListener(IPAddress.Any, port); //listen on all available network interfaces from the machine, and use the port specified by user
+                server.Start(); //start listening for incoming connection requests
+                txtLog.AppendText("Server started on port " + port + Environment.NewLine);
+
+                btnStartServer.Enabled = false;
+                btnStopServer.Enabled = true;
+
+                Task.Run(() => ListenForClient());  //start a new thread to listen for client connections, so that the UI thread is not blocked and can still respond to user interactions. Or meaning it works in the background, allowing the main thread to remain responsive.
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error starting server: " + ex.Message);
+            }
+            
+        }
+
+        private void ListenForClient()
+        //This method runs in a separate thread and continuously listens for incoming client connections.
+        //When a client connects, it accepts the connection, reads the message sent by the client, and updates the UI with the client's information and the received message.
+        //The use of Invoke is necessary to update the UI from a non-UI thread.
+        {
+            try
+            {
+                while (true)
+                {
+                    TcpClient client = server.AcceptTcpClient(); //This method blocks until there is connection and returns a TcpClient object that represents the connection to the client. It allows us to read from and write to the client.
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        txtLog.AppendText("Client connected from: " + client.Client.RemoteEndPoint + Environment.NewLine);
+                    });
+
+                    NetworkStream stream = client.GetStream(); 
+                    byte[] buffer = new byte[1024]; // Using [] to tell C# that this is an array, not a single byte
+                    int bytesRead = stream.Read(buffer, 0, buffer.Length);
+
+                    string receivedMessage = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    this.Invoke((MethodInvoker)delegate
+                    {
+                        txtLog.AppendText("Received Message: " + receivedMessage + Environment.NewLine);
+                    });
+
+                    client.Close();
+                }
+            }
+            catch
+            { 
+                //if stop, finished automatically
+            }
+        }
+
+        private void btnStopServer_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (server != null)
+                {
+                    server.Stop();
+                    server = null;
+                    txtLog.AppendText("Server stopped." + Environment.NewLine);
+                    btnStartServer.Enabled = true;
+                    btnStopServer.Enabled = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                txtLog.AppendText("Error during stop: " + ex.Message + Environment.NewLine);
+            }
+            
+        }
+
+    
+    }
+}
